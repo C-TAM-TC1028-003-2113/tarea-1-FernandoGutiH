f9982f7dfb9a0a86d5505e4ef18edaab0d672b88
FernandoGutiH <A01411566@itesm.mx>
2021-08-27 13:23:33 -0500

    Tarea Videojuegos

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    nuevo = int(input("Dame la cantidad de juegos nuevos: "))
+    usado = int(input("Dame la cantidad de juegos usados: "))
+    n = nuevo * 1000
+    u = usado * 350
+    n_u = n + u
+    print ("El total de la compra es:", n_u)
 
 
 if __name__ == '__main__':
