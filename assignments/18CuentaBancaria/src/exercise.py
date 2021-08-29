cc9d5d3d980c6cfdd9956f29deccf0a12e6c78f4
FernandoGutiH <A01411566@itesm.mx>
2021-08-28 22:55:59 -0500

    Tarea Cuenta Bancaria

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    mesant = float(input("Dame el saldo del mes anterior: "))
+    ingresos = float(input("Dame los ingresos: "))
+    egresos = float(input("Dame los egresos: "))
+    cheques = int(input("Dame el número de cheques: "))
+    final = (mesant + ingresos - egresos - (cheques * 13)) * 0.925
+    print("El saldo final de la cuentaes:", final)
 
 
 if __name__ == '__main__':
