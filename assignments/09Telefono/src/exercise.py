b493571a8006c65de67d2a7cd982e15db0cdde53
FernandoGutiH <A01411566@itesm.mx>
2021-08-31 11:52:28 -0500

    Tarea Telefono

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,6 +1,6 @@
 def main():
     # escribe tu código abajo de esta línea
-    mensaje = int(input("Dame el número de mensaje: "))
+    mensaje = int(input("Dame el número de mensajes: "))
     megas = float(input("Dame el número de megas: "))
     minutos = int(input("Dame el número de minutos: "))
     m = mensaje * 0.80 + megas * 0.80 + minutos * 0.80
